9eda9f00f2b0c3278aba97d21816665d94e5c401
Devanshu Srivastava <42748021+devanshusrivastava25@users.noreply.github.com>
2018-12-07 20:34:50 +0530

    Create lfr.ino

---
+++ lfr.ino
@@ -0,0 +1,55 @@
+/*------ Arduino Line Follower Code----- */
+/*-------definning Inputs------*/
+#define ir1 2      // left sensor
+#define ir2 3      // right sensor
+
+/*-------definning Outputs------*/
+#define LM1 4       // left motor
+#define LM2 5       // left motor
+#define RM1 6       // right motor
+#define RM2 7       // right motor
+
+void setup()
+{
+  pinMode(ir1, INPUT);
+  pinMode(ir2, INPUT);
+  pinMode(LM1, OUTPUT);
+  pinMode(LM2, OUTPUT);
+  pinMode(RM1, OUTPUT);
+  pinMode(RM2, OUTPUT);
+}
+
+void loop()
+{
+  if(digitalRead(ir1) && digitalRead(ir2))     // Move Forward
+  {
+    digitalWrite(LM1, HIGH);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, HIGH);
+    digitalWrite(RM2, LOW);
+  }
+  
+  if(!(digitalRead(ir1)) && digitalRead(ir2))     // Turn right
+  {
+    digitalWrite(LM1, LOW);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, HIGH);
+    digitalWrite(RM2, LOW);
+  }
+  
+  if(digitalRead(ir1) && !(digitalRead(ir2)))     // turn left
+  {
+    digitalWrite(LM1, HIGH);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, LOW);
+    digitalWrite(RM2, LOW);
+  }
+  
+  if(!(digitalRead(ir1)) && !(digitalRead(ir2)))     // stop
+  {
+    digitalWrite(LM1, LOW);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, LOW);
+    digitalWrite(RM2, LOW);
+  }
+}
